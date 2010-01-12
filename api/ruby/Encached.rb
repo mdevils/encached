02698d95b563e31a60c0a201a1ed28c67351a276
fantom-ua <fatnomcsm@gmail.com>
2010-01-13 01:04:05 +0200

    Another little Ruby API update

--- api/ruby/Encached.rb
+++ api/ruby/Encached.rb
@@ -1,22 +1,21 @@
 # Encached Ruby Client Library v0.1
 # Bogdan Golovchenko <fantomcsm@gmail.com>
 # twitter: fantom_ua
-# 10 january 2010
+# 13 january 2010
 
 require 'socket'
 
 class Encached
- attr_accessor :server, :port
+  attr_accessor :server, :port
 
 def initialize(server,port)
   @server = server
   @port = port
-  #puts "\r\n Connecting #{@server} on port: #{@port} \r\n"
   @s = TCPSocket.open(@server, @port)
 end
 
 def put(key, value)
-  @s.print("PUT #{key.to_s.length} #{key} #{value.to_s.length}\r\n#{value}\r\n")
+@s.print("PUT #{key.to_s.length} #{key.to_s} #{value.to_s.length}\r\n#{value.to_s}\r\n")
 
 case @s.gets.chomp
   when 'SUCCESS'
@@ -29,27 +28,30 @@ end
 end
 
 def get(key)
-  @s.print("GET #{key.to_s.length} #{key}\r\n")
-  response = @s.gets
+  @s.print("GET #{key.to_s.length} #{key.to_s}\r\n")
+  response = @s.gets.to_s
   resp, len, data = response.split
-  response = response.delete resp
-  response = response.delete len   
   response_len = response.lstrip.rstrip.length
  
 case resp
   when 'DATA'
   begin
-if response_len >= len.to_i
-else
-  until response_len > len.to_i do
-  response2 = @s.gets
-  response_len = response_len + response2.length
-  response = response + response2
+    response[0..3] = ''
+    response[0..len.length.to_i] = ''
+    response = response.gsub(/(^[A-Z]*1) +/, '')
+    
+  if response_len >= len.to_i
+  else
+    len_wait = len.to_i
+  until response_len > len_wait do
+      response2 = @s.gets.to_s
+      response_len = response_len + response2.length
+      response = response + response2
 end
 end      
   return response.lstrip
   end
-    when 'NODATA'
+  when 'NODATA'
     return false
 end
 end
--- api/ruby/example.rb
+++ api/ruby/example.rb
@@ -2,11 +2,15 @@ require 'Encached'
 
 cache = Encached.new('127.0.0.1', 2332)
 
-cache.put('hello',"Hello World")
-#cache.put('email123', "fantomcsm@gmail.com")
-
-#puts cache.get('email123')
-puts cache.get('hello')
 
 cache.remove('hello')
-#cache.remove('email123')
+cache.remove('email123')
+cache.remove('DATA 123')
+
+cache.put('hello',"Hello World")
+cache.put('email123', "fantomcsm@gmail.com")
+cache.put('DATA',"DATA 12345")
+
+puts cache.get('email123')
+puts cache.get('hello')
+puts cache.get('DATA')
